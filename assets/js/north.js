be91daafa359a20437e040a40a98c6b7abd5a34d
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-03-17 16:02:47 +0000

    North page places section cross device interactivity

--- assets/js/north.js
+++ assets/js/north.js
@@ -6,6 +6,15 @@ L.tileLayer('http://{s}.tile.osm.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(mymap);
 
+//The code bellow was adapted from https://stackoverflow.com/questions/23910594/leaflet-responsive-design-creating-different-zoom-levels-for-different-screen - however this only works after refreshing the page for some reason that I don't understand yet
+var width = screen.width;
+if (width < 541) {
+    mymap.setZoom(7);
+}  else {
+    mymap.setZoom(8);
+}
+//Here ends the code adapted from https://stackoverflow.com/questions/23910594/leaflet-responsive-design-creating-different-zoom-levels-for-different-screen
+
 //  MAP MARKERS - code written according to Leaflet documentation 
 
 var marker1 = L.marker([41.8, -8.25]).addTo(mymap)
