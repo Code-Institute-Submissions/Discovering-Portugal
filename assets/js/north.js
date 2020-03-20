2b7e17805e2af96972d7498cd59db25942cd8a2f
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-03-20 16:59:33 +0000

    Font-awesome icon on map pop-up, tips for the places and media query changing order of magazinequote with interests - all in the North Page

--- assets/js/north.js
+++ assets/js/north.js
@@ -18,23 +18,23 @@ if (width < 541) {
 //  MAP MARKERS - code written according to Leaflet documentation 
 
 var marker1 = L.marker([41.8, -8.25]).addTo(mymap)
-    .bindPopup('GERÊS <br> ???')
+    .bindPopup('<p style="text-align: center; font-size: 14px">PARQUE NACIONAL DA PENEDA-GERÊS</p> <p style="text-align:center"><i class="fas fa-tree" style="font-size: 16px"></i> <i class="fas fa-bed" style="font-size: 16px"></i> <i class="fas fa-campground" style="font-size: 16px"></i> <i class="fas fa-swimmer" style="font-size: 16px"></i> <i class="fas fa-hiking" style="font-size: 16px"></i></p>')
     .closePopup();
 
 var marker2 = L.marker([40.4, -7.55]).addTo(mymap)
-    .bindPopup('SERRA DA ESTRELA <br> ');
+    .bindPopup('<p style="text-align: center; font-size: 14px">SERRA DA ESTRELA</p> <p style="text-align:center"><i class="fas fa-tree" style="font-size: 16px"></i> <i class="fas fa-bed" style="font-size: 16px"></i> <i class="fas fa-campground" style="font-size: 16px"></i> <i class="fas fa-swimmer" style="font-size: 16px"></i> <i class="fas fa-utensils" style="font-size: 16px"></i> <i class="fas fa-hiking" style="font-size: 16px"></i></p>');
 
 var marker3 = L.marker([41.15, -8.61]).addTo(mymap)
-    .bindPopup('PORTO <br> ');
+    .bindPopup('<p style="text-align: center; font-size: 14px">PORTO</p> <p style="text-align:center"><i class="fas fa-city" style="font-size: 16px"></i> <i class="fas fa-bed" style="font-size: 16px"></i> <i class="fas fa-utensils" style="font-size: 16px"></i> <i class="fas fa-university" style="font-size: 16px"></i></p>');
 
 var marker4 = L.marker([40.64, -8.65]).addTo(mymap)
-    .bindPopup('AVEIRO <br> ');
+    .bindPopup('<p style="text-align: center; font-size: 14px">AVEIRO</p> <p style="text-align:center"><i class="fas fa-city" style="font-size: 16px"></i> <i class="fas fa-bed" style="font-size: 16px"></i> <i class="fas fa-utensils" style="font-size: 16px"></i> <i class="fas fa-university" style="font-size: 16px"></i></p>');
 
 var marker5 = L.marker([40.69, -8.73]).addTo(mymap)
-    .bindPopup('COSTA NOVA <br> ');
+    .bindPopup('<p style="text-align: center; font-size: 14px">COSTA NOVA</p> <p style="text-align:center"><i class="fas fa-umbrella-beach" style="font-size: 16px"></i> <i class="fas fa-bed" style="font-size: 16px"></i> <i class="fas fa-campground" style="font-size: 16px"></i> <i class="fas fa-swimmer" style="font-size: 16px"></i></p>');
 
 var marker6 = L.marker([41.85, -8.87]).addTo(mymap)
-    .bindPopup('PRAIA DO MOLEDO <br> ');
+    .bindPopup('<p style="text-align: center; font-size: 14px">PRAIA DO MOLEDO</p> <p style="text-align:center"><i class="fas fa-umbrella-beach" style="font-size: 16px"></i> <i class="fas fa-bed" style="font-size: 16px"></i> <i class="fas fa-campground" style="font-size: 16px"></i> <i class="fas fa-swimmer" style="font-size: 16px"></i></p>');
 
 // BUTTONS
 
