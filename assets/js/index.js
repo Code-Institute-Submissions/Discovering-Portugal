1dae45ad44820d8ea249762800ca75d4b8ee998b
Mariana Pereira <mariana.c.pereira.23@hotmail.com>
2020-03-25 13:50:52 +0000

    Index page Java-Script and some data in About Portugal section

---
+++ assets/js/index.js
@@ -0,0 +1,44 @@
+// BUTTONS
+
+$(document).ready(function(){
+    $("#btn1-index").click(function(){
+        $("#basics").removeClass('hidden');
+        $("#regions").addClass('hidden');
+        $("#history").addClass('hidden');
+        $("#accessibility").addClass('hidden');
+        $("#btn1-span").addClass('underline');
+        $("#btn2-span").removeClass('underline');
+        $("#btn3-span").removeClass('underline');
+        $("#btn4-span").removeClass('underline');
+    });
+    $("#btn2-index").click(function(){
+        $("#basics").addClass('hidden');
+        $("#regions").removeClass('hidden');
+        $("#history").addClass('hidden');
+        $("#accessibility").addClass('hidden');
+        $("#btn1-span").removeClass('underline');
+        $("#btn2-span").addClass('underline');
+        $("#btn3-span").removeClass('underline');
+        $("#btn4-span").removeClass('underline');
+    });
+    $("#btn3-index").click(function(){
+        $("#basics").addClass('hidden');
+        $("#regions").addClass('hidden');
+        $("#history").removeClass('hidden');
+        $("#accessibility").addClass('hidden');
+        $("#btn1-span").removeClass('underline');
+        $("#btn2-span").removeClass('underline');
+        $("#btn3-span").addClass('underline');
+        $("#btn4-span").removeClass('underline');
+    });
+    $("#btn4-index").click(function(){
+        $("#basics").addClass('hidden');
+        $("#regions").addClass('hidden');
+        $("#history").addClass('hidden');
+        $("#accessibility").removeClass('hidden');
+        $("#btn1-span").removeClass('underline');
+        $("#btn2-span").removeClass('underline');
+        $("#btn3-span").removeClass('underline');
+        $("#btn4-span").addClass('underline');
+    });
+});
